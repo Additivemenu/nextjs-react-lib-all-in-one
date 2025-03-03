8dabb2862befe88217b9b7190ec3ceed3ede484f
XUeshuo Li <spoonlee24k@gmail.com>
2025-03-03 11:08:04 +1100

    added a render-if wrapper

---
+++ components/wrappers/render-if/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+
+interface RenderIfProps {
+  condition: boolean;
+  children: ReactNode;
+}
+
+export const RenderIf = ({ condition, children }: RenderIfProps) => {
+  if (!condition) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
