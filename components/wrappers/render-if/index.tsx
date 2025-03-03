4e18c13baad22ff0680aeedec0b8b3e9a6f02995
Shawn Li <103415723+Additivemenu@users.noreply.github.com>
2025-03-03 12:41:02 +1100

    refined code review script (#17)
    * added some comments in tsx file
    
    * refined the code review bot script

--- components/wrappers/render-if/index.tsx
+++ components/wrappers/render-if/index.tsx
@@ -1,10 +1,16 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 interface RenderIfProps {
   condition: boolean;
   children: ReactNode;
 }
 
+/**
+ * wrapper component to conditionally render a component
+ *
+ * @param param0
+ * @returns
+ */
 export const RenderIf = ({ condition, children }: RenderIfProps) => {
   if (!condition) {
     return null;
